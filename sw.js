d75a3007ea6a81aacfa55f4246bb4cb5b380a044
Uzair Qamar <uzairqamarr@gmail.com>
2026-09-04 08:45:53 +1000

    update v2026.09.03b

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * with cache:"no-store". Without that, a push can take ten minutes to be
  * noticed even on a good connection.
  */
-var VERSION = "v2026.09.03a";
+var VERSION = "v2026.09.03b";
 var CACHE = "phar-" + VERSION;
 
 /* Relative so this keeps working if the repo or Pages path is ever renamed. */
