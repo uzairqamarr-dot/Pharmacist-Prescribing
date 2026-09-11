5f9edcfd6e6c6fbb73b83317ba2f33c9e4963f18
Uzair Qamar <uzairqamarr@gmail.com>
2026-09-12 03:19:19 +1000

    update v2026.09.12o

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    semester dashboard), and this worker controls the whole origin now, so
    they need to work offline the same as everything else. */
 "use strict";
-var VERSION = "v2026.09.12n";
+var VERSION = "v2026.09.12o";
 var CACHE = "phar-hub-" + VERSION;
 var ASSETS = [
   "./",
